d3ed75e1f55033951c63259fbaafb54eb5f5d7a4
Soham Metha <sohammetha01@gmail.com>
2025-07-08 20:52:14 +0000

    09/07/2025 : 02:22:14

--- src/VM/virex_ui.c
+++ src/VM/virex_ui.c
@@ -139,7 +139,7 @@ void CreateWindows()
 
 void setInputEnable(bool enable)
 {
-    
+    keypad(disp.windows[INPUT], enable);
 }
 
 void wprintdash(int id, int col)
@@ -201,7 +201,7 @@ void enterTUIMode()
                       "\n    Core libs(other)\t: SOHAM METHA  ");
 
     refreshAllWindows();
-    keypad(disp.windows[INPUT], true);
+    setInputEnable(true);
 }
 
 void dumpStack(const Memory *mem)
