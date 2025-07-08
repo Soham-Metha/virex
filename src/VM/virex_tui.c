e06a591cb93ebdafafd7a097be9b235b5fd5f3e4
Soham Metha <sohammetha01@gmail.com>
2025-07-08 19:44:27 +0000

    09/07/2025 : 01:14:27

--- src/VM/virex_tui.c
+++ src/VM/virex_tui.c
@@ -1,13 +1,2 @@
 #include "virex_tui.h"
 #include "sasm_instructions.h"
-
-static String WindowNames[MAX_WINDOW_COUNT] = {
-    [OUTPUT] = {.data = "OUTPUT", .length = 6},  [DETAILS] = {.data = "DETAILS", .length = 7},
-    [MEMORY] = {.data = "MEMORY", .length = 6},  [PROGRAM] = {.data = "PROGRAM", .length = 8},
-    [INPUT] = {.data = "INPUT", .length = 6},    [NAME] = {.data = "VIREX", .length = 5},
-    [CREDITS] = {.data = "CREDITS", .length = 7}};
-
-String getNameForWindow(int id)
-{
-    return WindowNames[id];
-}
