6c4720b569d6d11303b922ff1097293e33a4a1c5
Soham Metha <sohammetha01@gmail.com>
2025-07-08 19:36:37 +0000

    09/07/2025 : 01:06:37

--- src/VM/virex_ui.c
+++ src/VM/virex_ui.c
@@ -333,7 +333,7 @@ void printOut(int id, const char *str, ...)
 {
     va_list args;
     va_start(args, str);
-    wprintw(disp.windows[id], "1");
+    vwprintw(disp.windows[id], str, args);
     va_end(args);
 }
 
